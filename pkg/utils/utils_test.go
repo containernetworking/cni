897766d74b1455b1f5ab99bd9c4688cfef0c3368
Stefan Junker <mail@stefanjunker.de>
2016-03-31 17:11:11 +0200

    pkg/utils: correct the test's expected chain names

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -9,13 +9,13 @@ var _ = Describe("Utils", func() {
 	It("must format a short name", func() {
 		chain := FormatChainName("test", "1234")
 		Expect(len(chain)).To(Equal(maxChainLength))
-		Expect(chain).To(Equal("CNI-2bbe0c48b91a7d1b8a6753a8b"))
+		Expect(chain).To(Equal("CNI-2bbe0c48b91a7d1b8a6753a8"))
 	})
 
 	It("must truncate a long name", func() {
 		chain := FormatChainName("testalongnamethatdoesnotmakesense", "1234")
 		Expect(len(chain)).To(Equal(maxChainLength))
-		Expect(chain).To(Equal("CNI-374f33fe84ab0ed84dcdebe38"))
+		Expect(chain).To(Equal("CNI-374f33fe84ab0ed84dcdebe3"))
 	})
 
 	It("must be predictable", func() {
@@ -31,7 +31,7 @@ var _ = Describe("Utils", func() {
 		chain2 := FormatChainName("testalongnamethatdoesnotmakesense", "1235")
 		Expect(len(chain1)).To(Equal(maxChainLength))
 		Expect(len(chain2)).To(Equal(maxChainLength))
-		Expect(chain1).To(Equal("CNI-374f33fe84ab0ed84dcdebe38"))
+		Expect(chain1).To(Equal("CNI-374f33fe84ab0ed84dcdebe3"))
 		Expect(chain1).NotTo(Equal(chain2))
 	})
 })
