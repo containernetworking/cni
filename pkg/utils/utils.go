77759626a848d8a9fd3b4302fe97d043e31ef2e7
Stefan Junker <mail@stefanjunker.de>
2016-03-31 11:50:18 +0200

    pkg/utils: fix docstring

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -12,7 +12,7 @@ const (
 )
 
 // Generates a chain name to be used with iptables.
-// Ensures that the generated chain name is less than
+// Ensures that the generated chain name is exactly
 // maxChainLength chars in length
 func FormatChainName(name string, id string) string {
 	chainBytes := sha512.Sum512([]byte(name + id))
