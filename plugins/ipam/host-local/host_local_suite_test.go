2445a960a9aed231a1f9ebb932d254d9751cbaa1
Minhan Xia <Minhan.Xia@gmail.com>
2016-06-02 11:37:05 -0700

    plugins/ipam: round robin ip allocation for host-local ipam
    This changes the ip allocation logic to round robin. Before this, host-local IPAM searched for available IPs from start of subnet. Hence it tends to allocate IPs that had been used recently. This is not ideal since it may cause collisions.

--- plugins/ipam/host-local/host_local_suite_test.go
+++ plugins/ipam/host-local/host_local_suite_test.go
@@ -1,4 +1,4 @@
-// Copyright 2015 CNI authors
+// Copyright 2016 CNI authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package backend
+package main_test
 
-import "net"
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-type Store interface {
-	Lock() error
-	Unlock() error
-	Close() error
-	Reserve(id string, ip net.IP) (bool, error)
-	LastReservedIP() (net.IP, error)
-	Release(ip net.IP) error
-	ReleaseByID(id string) error
+	"testing"
+)
+
+func TestHostLocal(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "HostLocal Suite")
 }
