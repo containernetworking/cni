adf28a84c62fb0eb7f9949abd1f92dee729a6708
Gabe Rosenhouse <rosenhouse@gmail.com>
2016-08-29 19:20:18 -0400

    versioning: document meaning of 'Legacy' version support

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -79,4 +79,11 @@ func Decode(jsonBytes []byte) (PluginInfo, error) {
 	return &info, nil
 }
 
+// Legacy PluginInfo describes a plugin that is backwards compatible with the
+// CNI spec version 0.1.0.  In particular, a runtime compiled against the 0.1.0
+// library ought to work correctly with a plugin that reports support for
+// Legacy versions.
+//
+// Any future CNI spec versions which meet this definition will be added to
+// this list.
 var Legacy = PluginSupports("0.1.0", "0.2.0", "0.3.0")
