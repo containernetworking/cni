ce65830443caebd813926f4c1e8c8944b371ce0d
Roman Mohr <rmohr@redhat.com>
2016-09-12 15:24:35 +0200

    Initial macvtap support
    TODO Tests
    TODO Documentation

---
+++ plugins/main/macvtap/macvtap.go
@@ -0,0 +1,182 @@
+// Copyright 2015 CNI authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"runtime"
+
+	"github.com/containernetworking/cni/pkg/ip"
+	"github.com/containernetworking/cni/pkg/ns"
+	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/containernetworking/cni/pkg/types"
+	"github.com/vishvananda/netlink"
+	"github.com/containernetworking/cni/pkg/version"
+)
+
+type NetConf struct {
+	types.NetConf
+	Master string `json:"master"`
+	Mode   string `json:"mode"`
+	MTU    int    `json:"mtu"`
+	MAC    string `json:"mac"`
+}
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func loadConf(bytes []byte) (*NetConf, error) {
+	n := &NetConf{}
+	if err := json.Unmarshal(bytes, n); err != nil {
+		return nil, fmt.Errorf("failed to load netconf: %v", err)
+	}
+	if n.Master == "" {
+		return nil, fmt.Errorf(`"master" field is required. It specifies the host interface name to virtualize`)
+	}
+
+	return n, nil
+}
+
+func modeFromString(s string) (netlink.MacvlanMode, error) {
+	switch s {
+	case "", "bridge":
+		return netlink.MACVLAN_MODE_BRIDGE, nil
+	case "private":
+		return netlink.MACVLAN_MODE_PRIVATE, nil
+	case "vepa":
+		return netlink.MACVLAN_MODE_VEPA, nil
+	case "passthru":
+		return netlink.MACVLAN_MODE_PASSTHRU, nil
+	default:
+		return 0, fmt.Errorf("unknown macvlan mode: %q", s)
+	}
+}
+
+func createMacvtap(conf *NetConf, ifName string, netns ns.NetNS) error {
+	mode, err := modeFromString(conf.Mode)
+	if err != nil {
+		return err
+	}
+
+	m, err := netlink.LinkByName(conf.Master)
+	if err != nil {
+		return fmt.Errorf("failed to lookup master %q: %v", conf.Master, err)
+	}
+
+	// due to kernel bug we have to create with tmpName or it might
+	// collide with the name on the host and error out
+	tmpName, err := ip.RandomVethName()
+	if err != nil {
+		return err
+	}
+
+	mv := &netlink.Macvtap{
+		Macvlan: netlink.Macvlan{
+			LinkAttrs: netlink.LinkAttrs{
+				MTU:         conf.MTU,
+				Name:        tmpName,
+				ParentIndex: m.Attrs().Index,
+				Namespace:   netlink.NsFd(int(netns.Fd())),
+			},
+			Mode: mode,
+		},
+	}
+
+	if err := netlink.LinkAdd(mv); err != nil {
+		return fmt.Errorf("failed to create macvtap: %v", err)
+	}
+
+	return netns.Do(func(_ ns.NetNS) error {
+		err := renameLink(tmpName, ifName)
+		if err != nil {
+			_ = netlink.LinkDel(mv)
+			return fmt.Errorf("failed to rename macvtap to %q: %v", ifName, err)
+		}
+		return nil
+	})
+}
+
+func cmdAdd(args *skel.CmdArgs) error {
+	n, err := loadConf(args.StdinData)
+	if err != nil {
+		return err
+	}
+
+	netns, err := ns.GetNS(args.Netns)
+	if err != nil {
+		return fmt.Errorf("failed to open netns %q: %v", netns, err)
+	}
+	defer netns.Close()
+
+	if err = createMacvtap(n, args.IfName, netns); err != nil {
+		return err
+	}
+
+	if n.MAC != "" {
+		err = netns.Do(func(_ ns.NetNS) error {
+			macIf, err := netlink.LinkByName(args.IfName)
+			if err != nil {
+				return fmt.Errorf("failed to lookup new macvtapdevice %q: %v", args.IfName, err)
+			}
+
+			mac, err := net.ParseMAC(n.MAC)
+			if err != nil {
+				return fmt.Errorf("failed to read provided MAC address %q: %v", n.MAC, err)
+			}
+
+			if err = netlink.LinkSetHardwareAddr(macIf, mac); err != nil {
+				return fmt.Errorf("failed to add hardware addr to %q: %v", args.IfName, err)
+			}
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+	}
+
+	// TODO use a provided cgroup to allow access to all devices in that cgroup
+
+	result := &types.Result{}
+	return result.Print()
+}
+
+func cmdDel(args *skel.CmdArgs) error {
+
+	if args.Netns == "" {
+		return nil
+	}
+
+	return ns.WithNetNSPath(args.Netns, func(_ ns.NetNS) error {
+		return ip.DelLinkByName(args.IfName)
+	})
+}
+
+func renameLink(curName, newName string) error {
+	link, err := netlink.LinkByName(curName)
+	if err != nil {
+		return err
+	}
+
+	return netlink.LinkSetName(link, newName)
+}
+
+func main() {
+	skel.PluginMain(cmdAdd, cmdDel, version.Legacy)
+}
