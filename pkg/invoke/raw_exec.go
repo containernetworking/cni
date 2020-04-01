fc115a1fe386d8270ca4272113bd48b1b02f4a7e
Dan Williams <dcbw@redhat.com>
2020-04-01 11:53:42 -0500

    invoke: capture and return stderr if plugin exits unexpectedly
    The way raw_exec invokes the command doesn't actually pass back
    stderr, despite ExitError having that capability.  c.Run()
    internally calls c.Wait() but that doesn't capture stderr and
    insert it into the returned ExitError. So we have to do that
    ourselves.
    
    Fixes: https://github.com/containernetworking/cni/issues/732
    Fixes: https://github.com/containernetworking/cni/pull/759
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: asears <asears@users.noreply.github.com>

--- pkg/invoke/raw_exec.go
+++ pkg/invoke/raw_exec.go
@@ -31,34 +31,37 @@ type RawExec struct {
 
 func (e *RawExec) ExecPlugin(ctx context.Context, pluginPath string, stdinData []byte, environ []string) ([]byte, error) {
 	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
 	c := exec.CommandContext(ctx, pluginPath)
 	c.Env = environ
 	c.Stdin = bytes.NewBuffer(stdinData)
 	c.Stdout = stdout
-	c.Stderr = e.Stderr
+	c.Stderr = stderr
 	if err := c.Run(); err != nil {
-		return nil, pluginErr(err, stdout.Bytes())
+		return nil, e.pluginErr(err, stdout.Bytes(), stderr.Bytes())
 	}
 
+	// Copy stderr to caller's buffer in case plugin printed to both
+	// stdout and stderr for some reason. Ignore failures as stderr is
+	// only informational.
+	if e.Stderr != nil && stderr.Len() > 0 {
+		_, _ = stderr.WriteTo(e.Stderr)
+	}
 	return stdout.Bytes(), nil
 }
 
-func pluginErr(err error, output []byte) error {
-	if exitError, ok := err.(*exec.ExitError); ok {
-		emsg := types.Error{}
-		if len(output) == 0 {
-			if len(exitError.Stderr) == 0 {
-				emsg.Msg = "netplugin failed with no error message"
-			} else {
-				emsg.Msg = fmt.Sprintf("netplugin failed: %q", string(exitError.Stderr))
-			}
-		} else if perr := json.Unmarshal(output, &emsg); perr != nil {
-			emsg.Msg = fmt.Sprintf("netplugin failed but error parsing its diagnostic message %q: %v", string(output), perr)
+func (e *RawExec) pluginErr(err error, stdout, stderr []byte) error {
+	emsg := types.Error{}
+	if len(stdout) == 0 {
+		if len(stderr) == 0 {
+			emsg.Msg = fmt.Sprintf("netplugin failed with no error message: %v", err)
+		} else {
+			emsg.Msg = fmt.Sprintf("netplugin failed: %q", string(stderr))
 		}
-		return &emsg
+	} else if perr := json.Unmarshal(stdout, &emsg); perr != nil {
+		emsg.Msg = fmt.Sprintf("netplugin failed but error parsing its diagnostic message %q: %v", string(stdout), perr)
 	}
-
-	return err
+	return &emsg
 }
 
 func (e *RawExec) FindInPath(plugin string, paths []string) (string, error) {
