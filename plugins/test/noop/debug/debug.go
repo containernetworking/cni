6b6330d263e17925dc0d84059ecc035497152a20
zouyu <zouy.fnst@cn.fujitsu.com>
2020-11-20 11:36:33 +0800

    Fix package comment
    Signed-off-by: zouyu <zouy.fnst@cn.fujitsu.com>

--- plugins/test/noop/debug/debug.go
+++ plugins/test/noop/debug/debug.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// debug supports tests that use the noop plugin
+// Package debug supports tests that use the noop plugin
 package debug
 
 import (
