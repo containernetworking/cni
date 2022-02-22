3e49ce16b529f44933bf0637ea0856cb305507c2
John Howard <howardjohn@google.com>
2022-02-22 12:54:57 -0800

    Fix incorrect pointer inputs to `json.Unmarshal`
    See https://github.com/howardjohn/go-unmarshal-double-pointer for more
    info on why this is not safe and how this is detected.
    
    Signed-off-by: John Howard <howardjohn@google.com>

--- pkg/types/040/types_test.go
+++ pkg/types/040/types_test.go
@@ -348,7 +348,7 @@ var _ = Describe("040 types operations", func() {
 }`))
 
 		recovered := &types040.IPConfig{}
-		Expect(json.Unmarshal(jsonBytes, &recovered)).To(Succeed())
+		Expect(json.Unmarshal(jsonBytes, recovered)).To(Succeed())
 		Expect(recovered).To(Equal(ipc))
 	})
 
@@ -363,7 +363,7 @@ var _ = Describe("040 types operations", func() {
 	Context("when unmarshalling json fails", func() {
 		It("returns an error", func() {
 			recovered := &types040.IPConfig{}
-			err := json.Unmarshal([]byte(`{"address": 5}`), &recovered)
+			err := json.Unmarshal([]byte(`{"address": 5}`), recovered)
 			Expect(err).To(MatchError(HavePrefix("json: cannot unmarshal")))
 		})
 	})
